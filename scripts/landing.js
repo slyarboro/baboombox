68e150ce22c9c1f4d3f32ee2d8f3cc9379ecb5ae
Your Name <yourname@example.com>
2017-04-20 18:24:42 -0400

    DOM Scripting: Events CP(23): Complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,6 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-    var points = document.getElementsByClassName('point');
-
+    var animatePoints = function(points) {
       var revealPoint = function(i) {
          points[i].style.opacity = 1;
          points[i].style.transform = "scaleX(1) translateY(0)";
@@ -13,4 +12,21 @@ var animatePoints = function() {
         revealPoint(i);
       }
 
-};
+    };
+
+  window.onload = function() {
+    //  animate points automatically on tall screens where trigger-scroll defunct
+    if (window.innerHeight > 950) {
+      animatePoints(pointsArray);
+    }
+
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+    window.addEventListener("scroll", function(event) {
+      if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+        animatePoints(pointsArray);
+      }
+    });
+
+  }
