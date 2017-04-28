6724df922d04cfd55661523000c4692b28f38db1
Your Name <yourname@example.com>
2017-04-28 01:55:58 -0400

    DOM Scripting: Play/Pause I CP(26): Complete

--- scripts/album.js
+++ scripts/album.js
@@ -35,7 +35,7 @@ var albumMarconi = {
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
       '<tr class="album-view-song-item">'
-     + '   <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
      + '   <td class="song-item-title">' + songName + '</td>'
      + '   <td class="song-item-duration">' + songLength + '</td>'
      + '</tr>'
@@ -76,12 +76,25 @@ var setCurrentAlbum = function(album) {
 
 // checkpoint 26 => the console output will show moused-over elements fire event which eventually registers with the table's event listener
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+// Album button templates
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 
 window.onload = function() {
     setCurrentAlbum(albumBenFolds);
 
     songListContainer.addEventListener('mouseover', function(event) {
-      // target(prop) on event(obj) stores DOM element where event occurred, like so --
-      console.log(event.target);
+      // only target individual song rows during event delegation
+      if (event.target.parentElement.className === 'album-view-song-item') {
+        event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+      }
     });
+
+    for (var i = 0; i < songRows.length; i++) {
+      songRows[i].addEventListener('mouseleave', function(event) {
+        // selects first child (song-item-number) element
+        this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+      });
+    }
 };
