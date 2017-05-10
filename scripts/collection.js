5993a43cea0896048a277ddd112988974005eb32
Your Name <yourname@example.com>
2017-05-10 03:13:58 -0400

    collection.js: Refactor

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,6 @@
-var collectionItemTemplate =
+// action-oriented function names begin with a verb => *build*CollectionItemTemplate
+var buildCollectionItemTemplate = function() {
+  var template =
      '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
@@ -14,12 +16,31 @@ var collectionItemTemplate =
    + '</div>'
    ;
 
-window.onload = function() {
-     var collectionContainer = document.getElementsByClassName('album-covers')[0];
-     /* ensure clean slate; assign empty string to innerHTML property to clear its content */
-     collectionContainer.innerHTML = '';
+   // checkpoint 30
+   // variable-name change to *template*; no jQuery methods as of now, but potentially later
+   // to support said potential, wrap *template* in jQuery object (to future-proof it)
+  return $(template);
+
+  // template wrapped in function => returns markup string as jQuery object (aka "jQuery template")
+};
+
+// change *window.onload* to its jQuery equivalent
+$(window).load(function() {
+
+    // substitute DOM selection with shorter jQuery alternative
+    // when element section becomes jQuery object, variable name *collectionContainer* prefixed with $ (identifying jQuery-related variables)
+     var $collectionContainer = $('.album-covers');
+
+     // replaced *innerHTML* property with empty() method
+     // removing vanilla DOM scripting from property
+     // empty() method: empties/removes any text or other elements (from the element(s) it's called on)
+     $collectionContainer.empty();
 
      for (var i = 0; i < 12; i++) {
-       collectionContainer.innerHTML += collectionItemTemplate;
+       var $newThumbnail = buildCollectionItemTemplate();
+
+       // replacing += in the *for loop* using append() method
+       // with each loop, template content appended to collection container
+       $collectionContainer.append($newThumbnail);
      }
-};
+});
