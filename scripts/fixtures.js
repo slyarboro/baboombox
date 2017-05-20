5bdf0339ac0528cff3e2dc1748dc676049e71526
Your Name <yourname@example.com>
2017-05-20 00:50:19 -0400

    jQuery(32) scripts/fixtures.js Updated

--- scripts/fixtures.js
+++ scripts/fixtures.js
@@ -1,29 +1,44 @@
 var albumBenFolds = {
-    title: 'Whatever and Ever Amen',
+    name: 'Whatever and Ever Amen',
     artist: 'Ben Folds Five',
     label: '| 550 Music',
     year: '1997',
     albumArtUrl: 'assets/images/album_covers/04.png',
     songs: [
-      {title: 'Brick', duration: '4:53'},
-      {title: 'Battle of Who Could Care Less', duration: '3:16'},
-      {title: 'Kate', duration: '3:13'},
-      {title: 'One Angry Dwarf And 200 Solemn Faces', duration: '3:52'},
-      {title: 'Steven\'s Last Night In Town', duration: '3:27'},
+      {name: 'Brick', length: '4:53'},
+      {name: 'Battle of Who Could Care Less', length: '3:16'},
+      {name: 'Kate', length: '3:13'},
+      {name: 'One Angry Dwarf And 200 Solemn Faces', length: '3:52'},
+      {name: 'Steven\'s Last Night In Town', length: '3:27'},
+    ]
+};
+
+var albumPicasso = {
+    name: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { name: 'Blue', length: '4:26' },
+        { name: 'Green', length: '3:14' },
+        { name: 'Red', length: '5:01' },
+        { name: 'Pink', length: '3:21' },
+        { name: 'Magenta', length: '2:15' }
     ]
 };
 
 var albumMarconi = {
-    title: 'The Telephone',
+    name: 'The Telephone',
     artist: 'Guglielmo Marconi',
     label: 'EM',
     year: '1909',
     albumArtUrl: 'assets/images/album_covers/20.png',
     songs: [
-      {title: 'Hello, Operator?', duration: '1:01'},
-      {title: 'Ring, ring, ring', duration: '5:01'},
-      {title: 'Fits in your pocket', duration: '3:21'},
-      {title: 'Can you hear me now?', duration: '3:14'},
-      {title: 'Wrong phone number', duration: '2:15'},
+      {name: 'Hello, Operator?', length: '1:01'},
+      {name: 'Ring, ring, ring', length: '5:01'},
+      {name: 'Fits in your pocket', length: '3:21'},
+      {name: 'Can you hear me now?', length: '3:14'},
+      {name: 'Wrong phone number', length: '2:15'},
     ]
 };
